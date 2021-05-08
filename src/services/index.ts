031f8119e0092855169e9652c0cf2570e5473300
v1rtl <pilll.pl22@gmail.com>
2021-05-08 20:47:41 +0300

    missing cardano import

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,5 +3,6 @@ import { service as ethplorer } from './ethplorer'
 import { service as blockcypher } from './blockcypher'
 import { service as chainz } from './chainz'
 import { service as stablecoin } from './stablecoin'
+import { service as cardano } from './cardano'
 
-export default { ethplorer, tokenbalance, blockcypher, chainz, stablecoin }
+export default { ethplorer, tokenbalance, blockcypher, chainz, stablecoin, cardano }
